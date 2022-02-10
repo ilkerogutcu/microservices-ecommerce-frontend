2f0d1c8cb30781b98f00cdaab9a90f7f4594fbf5
ilkerogtc <ilkerogtc@gmail.com>
2022-02-10 22:45:39 +0300

    feat(login): Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import ProductDetail from "./pages/product-detail/ProductDetail";
 import ProductDiscover from "./pages/product-discover/ProductDiscover";
 import Register from "./pages/register/Register";
 
 function App() {
-  return <Register/>
+  return <Login/>
 }
 
 export default App;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Announcement from "../../compenents/announcement/Announcement";
+import Footer from "../../compenents/footer/Footer";
+import Navbar from "../../compenents/navbar/Navbar";
+import Navigation from "../../compenents/navigation/Navigation";
+import Newsletter from "../../compenents/newsletter/Newsletter";
+import "./login.css";
+
+function Login() {
+  return (
+    <div>
+      <Announcement message="50 TL Üstü Tüm Siparişlerde Kargo Bedava!" />
+      <Navbar />
+      <Navigation />
+      <div className="login">
+        <div className="login-container">
+          <h1 className="login__title">GİRİŞ YAP</h1>
+          <form>
+            <input
+              className="login__input"
+              type="email"
+              placeholder="E-posta Adresiniz"
+            />
+            <input
+              className="login__input"
+              type="text"
+              placeholder="Şifreniz"
+            />
+            <button>GİRİŞ YAP</button>
+          </form>
+        </div>
+      </div>
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+}
+
+export default Login;
